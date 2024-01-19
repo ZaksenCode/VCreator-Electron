51c2fa97c85d34aa6da9b621e8f685ff274b3e9c
Zaksen <maksimsidorkin2006@mail.ru>
2024-01-19 19:29:24 +0200

    random commit

--- src/main.js
+++ src/main.js
@@ -7,7 +7,6 @@ const createWindow = () => {
       width: 1024,
       height: 573,
       webPreferences: {
-        preload: path.join(__dirname, 'preload.js'),
         nodeIntegration: true,
         nodeIntegrationInWorker: true
       },
@@ -18,13 +17,10 @@ const createWindow = () => {
     win.setTitle("VCreator 0.2");
     win.setMenuBarVisibility(false);
     win.webContents.openDevTools();
+
+    //win.addListener("ready-to-show", addListeners());
   
     win.loadFile('src/index.html');
-
-    win.addListener("DOMContentLoaded", (event) => {
-      document.getElementById("browse-button").addListener("click", openContentPage("content-project"));
-      document.getElementById("block-button").addtListener("click", openContentPage("content-block"));
-    })
 }
 
 app.whenReady().then(() => {
@@ -42,18 +38,3 @@ app.on('window-all-closed', () => {
 try {
     require('electron-reloader')(module)
   } catch (_) {}
-
-  function openContentPage(id) {
-    hideAllPages()
-    page = document.getElementById(id);
-    content_project.classList.toggle("hide");
-}
-  
-function hideAllPages() {
-    content_project = document.getElementById("content-project");
-    content_block = document.getElementById("content-block");
-    content_script = document.getElementById("content-script");
-    content_project.classList.add("hide");
-    content_block.classList.add("hide");
-    content_script.classList.add("hide");
-}
--- src/scripts/pages.js
+++ src/scripts/pages.js
@@ -1,1 +1,21 @@
 
+export function addListeners()
+{
+    document.getElementById("browse-button").addListener("click", openContentPage("content-project"));
+    document.getElementById("block-button").addtListener("click", openContentPage("content-block"));
+}
+
+function openContentPage(id) {
+    hideAllPages()
+    page = document.getElementById(id);
+    content_project.classList.toggle("hide");
+}
+    
+function hideAllPages() {
+    content_project = document.getElementById("content-project");
+    content_block = document.getElementById("content-block");
+    content_script = document.getElementById("content-script");
+    content_project.classList.add("hide");
+    content_block.classList.add("hide");
+    content_script.classList.add("hide");
+}
