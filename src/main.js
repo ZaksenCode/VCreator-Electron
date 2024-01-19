e46001ff40c1898cba77fd528cc5443fe0c32968
Zaksen <maksimsidorkin2006@mail.ru>
2024-01-19 18:41:19 +0200

    add listnears

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
-const { app, BrowserWindow } = require('electron')
-const path = require("path")
-const fs = require("fs")
+const { app, BrowserWindow } = require('electron');
+const path = require("path");
+const fs = require("fs");
 
 const createWindow = () => {
     const win = new BrowserWindow({
@@ -20,6 +20,11 @@ const createWindow = () => {
     win.webContents.openDevTools();
   
     win.loadFile('src/index.html');
+
+    win.addListener("DOMContentLoaded", (event) => {
+      document.getElementById("browse-button").addListener("click", openContentPage("content-project"));
+      document.getElementById("block-button").addtListener("click", openContentPage("content-block"));
+    })
 }
 
 app.whenReady().then(() => {
@@ -37,3 +42,18 @@ app.on('window-all-closed', () => {
 try {
     require('electron-reloader')(module)
   } catch (_) {}
+
+  function openContentPage(id) {
+    hideAllPages()
+    page = document.getElementById(id);
+    content_project.classList.toggle("hide");
+}
+  
+function hideAllPages() {
+    content_project = document.getElementById("content-project");
+    content_block = document.getElementById("content-block");
+    content_script = document.getElementById("content-script");
+    content_project.classList.add("hide");
+    content_block.classList.add("hide");
+    content_script.classList.add("hide");
+}
